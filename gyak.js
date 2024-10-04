1732a2daddc4c4611e1b52a17af139c7e35c0352
Szekeadam <98951578+Szekeadam@users.noreply.github.com>
2024-10-04 11:34:38 +0200

    Add files via upload

---
+++ gyak.js
@@ -0,0 +1,13 @@
+let szo="példa szöveg"
+console.log(szo.slice(0,5));
+console.log(szo.slice(3,8));
+console.log(szo.slice(5));
+console.log(szo.slice(5,-1));
+console.log (szo.toUpperCase());
+function ngybetu(str){
+    let masodik="";
+    for (let i = 0; i < str.length; i++) {
+        
+        
+    }
+}
